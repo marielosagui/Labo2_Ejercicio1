5e56c52b782ca0ce8f48f31de71f7c6b902e77aa
marielosagui <00078913@uca.edu.sv>
2019-03-15 08:11:13 -0600

    Ejercicio

---
+++ app/src/main/java/com/example/labo2/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.labo2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mLinearLayout = findViewById(R.id.ll1);
+
+        btn1=findViewById(R.id.btn1);
+
+        btn1.setOnCLickListener(this)
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        int viewId = v.getId();
+        Log.i(tag:"id", msg:viewId + "");
+        switch (viewId){
+            case R.id.btn1;
+                Toast.makeText(context:MainActivity.this , text "btn1", Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+    }
+}
